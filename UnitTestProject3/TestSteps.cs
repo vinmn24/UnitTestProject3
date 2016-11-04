b013abfec0bde1481164e088d09916978a5528e6
vinmn24 <vnaganur@spiderlogic.com>
2016-11-04 11:25:03 +0530

    After the logic

---
+++ UnitTestProject3/TestSteps.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace UnitTestProject3
+{
+    [Binding]
+    public class TestSteps
+    {
+        int no1, no2, total;
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number1)
+        {
+            no1 = number1;
+        }
+
+        [Given(@"we have also entered (.*) into the calculator")]
+        public void GivenWeHaveAlsoEnteredIntoTheCalculator(int number2)
+        {
+            no2 = number2;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            total = no1 + no2;
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int ExpectedTotal)
+        {
+            Assert.AreEqual(ExpectedTotal,total);
+        }
+    }
+}
